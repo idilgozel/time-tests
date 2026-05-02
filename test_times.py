4af8fef4b3ab36ac63e5e004640af566dc8d2bfc
idilgozel <ucapgoz@ucl.ac.uk>
2026-05-02 17:46:29 +0100

    all commit

--- test_times.py
+++ test_times.py
@@ -3,8 +3,9 @@ from times import compute_overlap_time
 from times import time_range
 
 
+import yaml
 
-    
+     
 def test_given_input():
     
 
@@ -58,3 +59,89 @@ def test_several_times_ranges():
 
 
 
+
+
+def test_from_yaml():
+    with open("fixture.yaml", "r") as file:
+        test_cases = yaml.safe_load(file)
+
+    for case in test_cases:
+        name = list(case.keys())[0]
+        data = case[name]
+
+        result = compute_overlap_time(
+            data["time_range_1"],
+            data["time_range_2"],
+        )
+
+        expected = [tuple(interval) for interval in data["expected"]]
+
+        assert result == expected
+
+
+url ="https://api.n2yo.com/rest/v1/satellite/visualpasses/25544/56/0/0/5/50&apiKey={VJR5AF-GDAKJ5-DFZ7EG-5QEA}"
+
+
+def iss_passes():
+    fake_response = {
+    "status_code": 200,
+    "json": lambda: {
+        "large": [("2010-02-12 10:00:00", "2010-02-12 12:00:00")],
+        "short": [("2010-02-12 10:30:00", "2010-02-12 10:45:00")],
+        "expected": [("2010-02-12 10:30:00", "2010-02-12 10:45:00")],
+    },
+    }
+    with path("times.get_response") as mock_get:
+        mock_get.return_value.json.return_value = fake_response
+        result = get_response()
+
+        assert result== fake_response
+
+    mock_get.assert_called_once_with(url)
+
+
+
+url ="https://api.n2yo.com/rest/v1/satellite/visualpasses/25544/56/0/0/5/50&apiKey={VJR5AF-GDAKJ5-DFZ7EG-5QEA}"
+
+
+def iss_passes():
+    fake_response = {
+    "status_code": 200,
+    "json": lambda: {
+        "large": [("2010-02-12 10:00:00", "2010-02-12 12:00:00")],
+        "short": [("2010-02-12 10:30:00", "2010-02-12 10:45:00")],
+        "expected": [("2010-02-12 10:30:00", "2010-02-12 10:45:00")],
+    },
+    }
+    with patch("times.get_response") as mock_get:
+        mock_get.return_value.json.return_value = fake_response
+        result = get_response()
+
+        assert result== fake_response
+
+    mock_get.assert_called_once_with(url)
+
+
+
+from unittest.mock import patch
+
+
+url ="https://api.n2yo.com/rest/v1/satellite/visualpasses/25544/56/0/0/5/50&apiKey={VJR5AF-GDAKJ5-DFZ7EG-5QEA}"
+
+
+def test_iss_passes():
+    fake_response = {
+    "status_code": 200,
+    "json": lambda: {
+        "large": [("2010-02-12 10:00:00", "2010-02-12 12:00:00")],
+        "short": [("2010-02-12 10:30:00", "2010-02-12 10:45:00")],
+        "expected": [("2010-02-12 10:30:00", "2010-02-12 10:45:00")],
+    },
+    }
+    with patch("times.get_response") as mock_get:
+        mock_get.return_value.json.return_value = fake_response
+        result = get_response()
+
+        assert result== fake_response
+
+    mock_get.assert_called_once_with(url)
--- times.py
+++ times.py
@@ -31,3 +31,13 @@ if __name__ == "__main__":
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     print(compute_overlap_time(large, short))
+
+
+def get_response():
+
+    url ="https://api.n2yo.com/rest/v1/satellite/visualpasses/25544/56/0/0/5/50&apiKey={VJR5AF-GDAKJ5-DFZ7EG-5QEA}"
+    response = requests.get(url)
+    return response.json()
+
+
+
