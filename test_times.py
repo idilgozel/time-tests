a0995d3dcebd44dd221e502b531d5eea848ef80a
idilgozel <ucapgoz@ucl.ac.uk>
2026-05-02 15:27:33 +0100

    commit all

--- test_times.py
+++ test_times.py
@@ -19,3 +19,42 @@ def test_given_input():
 ]
 
     assert result == expected
+    
+
+
+def test_no_overlap():
+
+    large = time_range("2010-02-12 10:00:00", "2010-02-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    expected=[]
+    result= times.compute_overlap_time(large, short)
+    assert expected==result
+
+
+
+def test_boundaries():
+
+    large = time_range("2010-02-12 10:00:00", "2010-02-12 12:00:00")
+    short = time_range("2010-02-12 12:00:00", "2010-02-12 12:04:00", 2, 60)
+    expected=[]
+    result= times.compute_overlap_time(large, short)
+    assert expected==result
+
+
+
+def test_several_times_ranges():
+    large = time_range("2010-02-12 10:00:00", "2010-02-12 12:00:00", 3, 300)
+    short = time_range("2010-02-12 10:20:00", "2010-02-12 11:40:00", 2, 600)
+
+    expected = [
+    ("2010-02-12 10:20:00", "2010-02-12 10:36:40"),
+    ("2010-02-12 10:41:40", "2010-02-12 10:55:00"),
+    ("2010-02-12 11:05:00", "2010-02-12 11:18:20"),
+    ("2010-02-12 11:23:20", "2010-02-12 11:40:00"),
+    ]
+    result= times.compute_overlap_time(large, short)
+    assert expected==result
+
+
+
+
